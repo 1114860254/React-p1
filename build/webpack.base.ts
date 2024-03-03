9eb0bda18526192840f9c4ddd6014298bbda1461
pecado <1114860254@qq.com>
2024-03-03 17:36:02 +0800

    commit

--- build/webpack.base.ts
+++ build/webpack.base.ts
@@ -1,16 +1,16 @@
-import { Configuration } from "webpack";
+import { Configuration, DefinePlugin } from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import { DefinePlugin } from "webpack";
+import * as dotenv from "dotenv";
 
-// import * as dotenv from "dotenv";
-console.log('NODE_ENV',process.env.NODE_ENV)
-console.log('BASE_ENV',process.env.BASE_ENV)
 const path = require("path");
- const webpack = require('webpack')
+
+console.log('NODE_ENV', process.env.NODE_ENV)
+console.log('BASE_ENV', process.env.BASE_ENV)
+
 // 加载配置文件
-// const envConfig = dotenv.config({
-//   path: path.resolve(__dirname, "../env/.env." + process.env.BASE_ENV),
-// });
+const envConfig = dotenv.config({
+  path: path.resolve(__dirname, "../env/.env." + process.env.BASE_ENV),
+});
 
 const baseConfig: Configuration = {
   entry: path.join(__dirname, "../src/index.tsx"), // 入口文件
@@ -35,13 +35,18 @@ const baseConfig: Configuration = {
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js"],
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".less", ".css"],
+    // 别名需要配置两个地方，这里和 tsconfig.json
+    alias: {
+      "@": path.join(__dirname, "../src"),
+    },
+    // modules: [path.resolve(__dirname, "../node_modules")], // 查找第三方模块只在本项目的node_modules中查找
   },
-  // plugins
+  // plugins 的配置
   plugins: [
     new HtmlWebpackPlugin({
       title: "webpack5-react-ts",
-      filename: "index-html",
+      filename: "index.html",
       // 复制 'index.html' 文件，并自动引入打包输出的所有资源（js/css）
       template: path.join(__dirname, "../public/index.html"),
       inject: true, // 自动注入静态资源
@@ -52,19 +57,16 @@ const baseConfig: Configuration = {
         removeAttributeQuotes: true,
         collapseWhitespace: true, //去空格
         removeComments: true, // 去注释
-        minifyJS: true, //在脚本元素和事件属性中缩小JavaScript(使用UglifyJS)
-        minifyCSS: true,//缩小CSS样式元素和样式属性
+        minifyJS: true, // 在脚本元素和事件属性中缩小JavaScript(使用UglifyJS)
+        minifyCSS: true, // 缩小CSS样式元素和样式属性
       },
-      nodeModules:path.resolve(__dirname,"../node_modules"),
+      nodeModules: path.resolve(__dirname, "../node_modules"),
     }),
     new DefinePlugin({
-      // "process.env": JSON.stringify(envConfig.parsed),
+      "process.env": JSON.stringify(envConfig.parsed),
       "process.env.BASE_ENV": JSON.stringify(process.env.BASE_ENV),
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
     }),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env)
-    })
   ],
 };
 
